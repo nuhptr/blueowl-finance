f91b4e20d95eb81c63036c22887e8bdcbdfe50a6
nuhptr <nugrahaadi733@gmail.com>
2024-05-20 09:02:39 +0700

    build: :truck: favicon move to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,32 @@
+export const dynamic = "force_dynamic" // for testing only
+
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import { Inter, IBM_Plex_Serif } from "next/font/google"
 
 import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] })
+// font initialization
+const inter = Inter({
+   subsets: ["latin"],
+   variable: "--font-inter", // custom variable name
+})
+const ibmPlexSerif = IBM_Plex_Serif({
+   subsets: ["latin"],
+   weight: ["400", "700"],
+   variable: "--font-ibm-plex-serif", // custom variable name
+})
 
 export const metadata: Metadata = {
    title: "Horizon | Banking",
    description:
       "Horizon is a modern banking platform for everyone. Integrated with the latest technologies to provide a seamless experience. Join us today!",
+   icons: { icon: "/icons/ic_logo.svg" }, // favicon
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
    return (
       <html lang="en">
-         <body className={inter.className}>{children}</body>
+         <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
       </html>
    )
 }
