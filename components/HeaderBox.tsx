e25135cf9ea074fec1248e64a4f182f202f434d0
nuhptr <nugrahaadi733@gmail.com>
2024-05-21 08:07:15 +0700

    build: :hammer: add new components

---
+++ components/HeaderBox.tsx
@@ -0,0 +1,15 @@
+/** `type = "title"` and `user` props are optional */
+export default function HeaderBox({ type = "title", title, subtext, user }: HeaderBoxProps) {
+   return (
+      <div className="header-box">
+         <h1 className="header-box-title">
+            {title}
+            {type === "greeting" && (
+               // &nbsp; is a non-breaking space
+               <span className="text-bankGradient">&nbsp;{user}</span>
+            )}
+         </h1>
+         <p className="header-box-subtext">{subtext}</p>
+      </div>
+   )
+}
